e07cc5bef4b215f05509c01675f06209d00a8f50
qxxnn04 <500577@chrome.escg.ac.uk>
2020-11-26 16:10:44 +0000

    Enemy AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,25 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    private Player player;
 
-
-    public int life = 0;
+    public int life = 5;
 
     public int damage = 5;
+
+    private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(player.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        agent.SetDestination(player.transform.position);
     }
     public void OnCollisionEnter(Collision col)
     {
